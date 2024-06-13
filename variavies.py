cd72fc2aef74b2e382cd637fdf6c87fdf08c63aa
Bowie <ianlopes2069@gmail.com>
2024-06-13 11:40:37 -0300

    add new file

---
+++ variavies.py
@@ -0,0 +1,36 @@
+# > Variáveis 
+
+# Uma variavél guarda volores.
+
+# 1. Primeira variável:
+
+idade = 26 
+
+print(idade)
+
+nome = 'Ian de Jesus'
+
+print(nome)
+
+"""
+    Tipos de variáveis
+
+    1. int: n° inteiros, ou seja. n° sem parte decimal: 0, 5, -100
+    2. float: números reais, ou seja, números com parte decimal: 1.0, -2.7, 3.14
+    3. str: cadeias de caracteres, ou seja, dados textuais (string)
+    4. bool: valores lógicos (booleanos): True ou False
+"""
+
+idade = 24
+altura = 1.7
+nome = 'Ian de Jesus'
+estudando = True
+
+print(type(idade))
+print(type(altura))
+print(type(nome))
+print(type(estudando))
+
+linguagerm = input('Qual a linguagem de programação que vc está estudando? ')
+
+print(f'A linguagem que vc está estudando é: {linguagerm}')
