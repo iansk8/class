e8e6df426349312849e7f2a52c7eee105dcc8e1f
Bowie <ianlopes2069@gmail.com>
2024-06-13 11:37:30 -0300

    add new file

---
+++ entrada e saída de dados.py
@@ -0,0 +1,8 @@
+# O comando print() é uma função para imprimir um valor no console.
+
+print('Helo, world!')
+print('Seja bem vindo ao curso de python do coding tank!')
+
+# O comando input() recebe a entrada de valores.
+
+input('Qual a lingua de programação que você está estudando ?')
