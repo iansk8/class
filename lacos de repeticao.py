27d872e3ddca77fd4e6b3016071bef2bad0d6197
Bowie <ianlopes2069@gmail.com>
2024-06-14 12:12:13 -0300

    add new file

--- lacos de repeticao.py
+++ lacos de repeticao.py
@@ -1,5 +1,9 @@
 # > Laços de repetição:
 
+# while
+
+# Exemplo 01:
+
 import random as rd
 
 n = rd.randint(1, 2)
@@ -12,3 +16,39 @@ while n_user != n:
     n_user = int(input('Escolha um número entre 1 e 2: '))
     
 print('Você acertou, parabens!')
+
+# Exemplo 02: Estrutura com contador.
+
+contador = 0
+
+while contador < 5:
+    print(contador)
+    contador += 1 
+
+# for:
+
+# Exemplo 01:
+
+for i in range(10):
+    print(i)
+
+# Explorando a função range():
+
+# Exemplo 02: com dois parâmetros:
+for i in range(1, 6):
+    print(i)
+
+# Exemplo 02: com dois parâmetros:
+for i in range(1, 14, 3):
+    print(i)
+
+
+# Exemplo prático:
+
+sum_nota = 0
+
+for i in range(3):
+    nota = float(input(f'Informe sua nota{i}: '))
+    sum_nota =+ nota
+
+print(f'Sua média foi: {sum_nota/3}')
